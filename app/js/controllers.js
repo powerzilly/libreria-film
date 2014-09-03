4278a83c5b6f26ba26fc0d04e56ac6d0c6b9a254
root <root@zilloServer.Home>
2014-09-04 01:30:50 +0200

    Creazione del controller e dell'index base

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,19 @@
 'use strict';
 
 /* Controllers */
+
+var filmApp = angular.module('filmApp', []);
+
+filmApp.controller('FilmListCtrl', function($scope) {
+  $scope.films = [
+    {'name': 'Film 1',
+     'snippet': 'Descrizione film 1',
+	 'photo': 'img/img1.jpg'},
+    {'name': 'Film 2',
+     'snippet': 'Descrizione film 2',
+	 'photo': 'img/img1.jpg'},
+    {'name': 'Film 3',
+     'snippet': 'Descrizione film 3',
+	 'photo': 'img/img1.jpg'}
+  ];
+});
