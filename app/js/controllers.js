aa040220cef7b899093889a20fc1fe53c38b8c07
powerzilly <ziliani.daniele@hotmail.it>
2014-09-05 14:16:55 +0200

    Implementazione Dependency injector

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,16 +4,9 @@
 
 var filmApp = angular.module('filmApp', []);
 
-filmApp.controller('FilmListCtrl', function($scope) {
-  $scope.films = [
-    {'name': 'Film 1',
-     'snippet': 'Descrizione film 1',
-	 'photo': 'img/img1.jpg'},
-    {'name': 'Film 2',
-     'snippet': 'Descrizione film 2',
-	 'photo': 'img/img1.jpg'},
-    {'name': 'Film 3',
-     'snippet': 'Descrizione film 3',
-	 'photo': 'img/img1.jpg'}
-  ];
+filmApp.controller('FilmListCtrl', function($scope, $http) {
+    $http.get('films/films.json').success(function(data) {
+  $scope.films = data;
+        });
+                                          
 });
